cdf90ab4722c41c354ca9b355ae7bf0bbd4df778
Radhika Garg <142100060+Radhikagarg20@users.noreply.github.com>
2023-10-18 20:28:26 +0530

    Update Url.py

--- Url.py
+++ Url.py
@@ -1,1 +1,16 @@
+import pyshorteners
+
+def shorten_url(url):
+    shortener = pyshorteners.Shortener()
+    shortened_url = shortener.tinyurl.short(url)
+    return shortened_url
+
+# Provide a different long URL for testing
+long_url = "https://www.example.com/some/long/url"
+
+# Call the URL shortener function
+short_url = shorten_url(long_url)
+
+# Print the shortened URL
+print("Shortened URL:", short_url)
 
